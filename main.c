49edad9d98fb5e06e7a37beb4377212547783bdd
김성현 <khyun8576@naver.com>
2023-10-12 15:46:14 +0900

    2

--- main.c
+++ main.c
@@ -3,16 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star(){
-	int i;
-	for(i=0; i<10; i++)
-		printf("*");
+void func1(void) {
+	int x;
+	printf("func1 x is at %p\n", &x);
 }
-
-int main(void){
-	print_star();
-	print_star();
-	print_star();
-	
+int main(void) {
+	int x;
+	printf("main x is at %p\n", &x);
+	func1();
 	return 0;
 }
